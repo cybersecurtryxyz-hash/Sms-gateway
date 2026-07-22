5f112813045518ac934011fa00bdaf8abfcf7bca
cybersecurtryxyz-hash <cyber.securtry.xyz@gmail.com>
2026-07-22 13:18:21 +0530

    Delete app directory

--- app/app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pages import pages_bp
-from .admin import admin_bp
-from .coworker import coworker_bp
-from .device import device_bp
-
-__all__ = ["pages_bp", "admin_bp", "coworker_bp", "device_bp"]
